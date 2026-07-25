10defcb42b779dd6a7a8a456ba055393fbe13767
siliconvalleyar-oss <siliconvalleyar@gmail.com>
2026-07-25 16:04:58 -0300

    isolate v1.0.6: electrocardiogram

--- epaper_success_v1.0.1/libs/tyme/tyme.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cstdint>
-
-namespace TYME {
-    void delay(uint32_t ms);
-    void delay_ms(uint32_t ms);
-    void delay_us(uint32_t us);
-    void delay_s(uint32_t s);
-}
