5be70d1873b9f46f5462d919cf75ca59f9195b2d
siliconvalleyar-oss <siliconvalleyar-oss@users.noreply.github.com>
2026-09-16 15:29:35 -0300

    chore: remove unrelated project files from Whater branch

--- RASPI_EPD_SRC_QR_Invert_Graphics/libs/app/config.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// Detección de arquitectura: 32 vs 64 bits
-#if (defined(__SIZEOF_POINTER__) && (__SIZEOF_POINTER__ == 4))
-    #define CPU_32_BITS
-#else
-    #define CPU_64_BITS
-#endif
